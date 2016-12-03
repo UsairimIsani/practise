d27f1f285d94595e0ee83c76db1d2a3d850bef57
Usairim Isani <UsairimIsani@users.noreply.github.com>
2016-12-03 10:30:53 +0500

    Word Reserver & Word Finder

---
+++ Usairim/main.js
@@ -0,0 +1,19 @@
+// function reverser() {
+//     var inp = document.getElementById("reverser").value;
+//     var temp = "";
+//     for (var i = inp.length - 1; i >= 0; i--) {
+//         temp += inp[i];
+//     }
+//     inp = temp;
+//     document.write(inp);
+// }
+// function finder() {
+//     var find1 = document.getElementById("wordFinder");
+//     var find = document.getElementById("wordFinder").value;
+//     var toFind = document.getElementById("wordToFind").value;
+//     for (var i = 0; i < find.length; i++) {
+//         var found = find.indexOf(toFind);
+//         var foundWord = find.slice(found, (toFind.length + found));
+//     }
+//     document.write(foundWord);
+// }
